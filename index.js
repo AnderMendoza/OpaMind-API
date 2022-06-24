b8191ce8e49282d528f0e9fb781c7fb795e4e47f
Anderson Mendoza <davidmramos53@gmail.com>
2022-06-24 12:13:40 -0500

    modifying data

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ var port = process.env.port || 3999;
 
 //prueba de conexion a la bd
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://santiago:archipielagoM1@cluster0.ujtyy.mongodb.net/?retryWrites=true&w=majority'
-                ,{ useNewUrlParser: true})
+mongoose.connect('mongodb+srv://santiago:archipielagoM1@cluster0.ujtyy.mongodb.net/?retryWrites=true&w=majority',
+                { useNewUrlParser: true})
                 .then(
                     ()=>{
-                        console.log('La conexion a la bd es correcto VAMOS PERU RUMBO AL MUNDIAL');
+                        console.log('La conexion a la bd es correcto');
                         //CREAR EL SERVIDOR
                         app.listen(port, ()=>{
-                            console.log('El servidor http://localhost:3999 esta funcionando.');
+                            console.log('El servidor esta funcionando.');
                         })
                     }
                 )
