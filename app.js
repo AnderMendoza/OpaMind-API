f55511b13b8e69f06a97f09e717673db0dff7e19
Anderson Mendoza <davidmramos53@gmail.com>
2022-06-29 09:50:48 -0500

    changes for deploy in keroku

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var techno_routes = require('./routes/techno');
 var home_music_routes = require('./routes/home_music');
 var reporte_routes = require('./routes/reporte');
 var reporterock_routes = require('./routes/reporterock');
-var reportejazz_routes = require('./routes/reportejazz');
 var reporte_home_music =  require('./routes/reporte_home_music');
 
 //MIDDLEWARE
@@ -41,7 +40,6 @@ app.use('/api/',techno_routes);
 app.use('/api/',home_music_routes);
 app.use('/api/',reporte_routes);
 app.use('/api/',reporterock_routes);
-app.use('/api/',reportejazz_routes);
 app.use('/api/',reporte_home_music);
 
 //EXPORTAR MODULO
